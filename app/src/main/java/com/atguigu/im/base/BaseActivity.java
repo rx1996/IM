aefb52fb2dbc95e925308b7f17c2af279b16fc24
rx1996 <3343108599@qq.com>
2017-07-01 16:20:57 +0800

    二 :   欢迎界面跳转及登录界面布局的完成

---
+++ app/src/main/java/com/atguigu/im/base/BaseActivity.java
@@ -0,0 +1,34 @@
+package com.atguigu.im.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Administrator on 2017/7/1.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+        ButterKnife.bind(this);
+        initView();
+        initData();
+        initListener();
+    }
+
+    public abstract void initListener();
+
+    public abstract void initData();
+
+    public void initView(){
+
+    };
+
+    public abstract int getLayoutId();
+}
